0daf26440b80c709985b128b783aa4f3a1984dc2
Jiang <15328044115>
2020-08-27 16:03:21 +0800

    根据oc2mangoLib调整OCRunner.

--- OCRunner/ORInterpreter.m
+++ OCRunner/ORInterpreter.m
@@ -14,33 +14,59 @@
 #import "MFValue.h"
 #import "ORStructDeclare.h"
 #import "ORSystemFunctionTable.h"
+#import "ORTypeVarPair+TypeEncode.h"
 @implementation ORInterpreter
 + (void)excute:(NSString *)string{
     //添加内置结构体、函数、变量等
     mf_add_built_in();
     MFScopeChain *scope = [MFScopeChain topScope];
     AST *abstractAst = [OCParser parseSource:string];
-    //注册Protcol
-    for (id <OCExecute> protcol in abstractAst.protcolCache.allValues){
-        [protcol execute:scope];
-    }
-    //注册Class
-    for (id <OCExecute> clazz in abstractAst.sortClasses){
-        [clazz execute:scope];
-    }
-    //执行全局变量，全局函数声明
-    NSMutableArray <ORTypeVarPair *>*funcVars = [NSMutableArray array];
-    NSMutableArray *names = [NSMutableArray array];
-    for (id <OCExecute> expression in abstractAst.globalStatements) {
-        if ([expression isKindOfClass:[ORDeclareExpression class]]) {
-            ORTypeVarPair *pair = [(ORDeclareExpression *)expression pair];
-            if ([pair.var isKindOfClass:[ORFuncVariable class]]) {
-                [funcVars addObject:pair];
-                [names addObject:pair.var.varname];
+    
+    //执行Class, Protcol, 全局变量，全局函数声明, typdef, struct, enum等
+    for (ORNode *node in abstractAst.nodes){
+        //跳过系统函数声明，防止向全局作用域注册变量
+        if ([node isKindOfClass:[ORDeclareExpression class]]) {
+            ORTypeVarPair *pair = [(ORDeclareExpression *)node pair];
+            if ([pair.var isKindOfClass:[ORFuncVariable class]]
+                && !pair.var.isBlock
+                && pair.var.ptCount == 0) {
                 continue;
             }
         }
-        [expression execute:scope];
+        [node execute:scope];
+    }
+    
+    //获取系统函数指针
+    [self linkSystemFunctionsWithNodes:abstractAst.nodes scope:scope];
+    
+    //执行main函数
+    MFValue *mainValue = [scope getValueWithIdentifier:@"main"];
+    if (mainValue != nil && [mainValue.objectValue isKindOfClass:[ORFunctionImp class]]) {
+        ORFunctionImp *mainFunction = mainValue.objectValue;
+        //模拟传参
+        NSMutableArray *args = [NSMutableArray array];
+        for (ORTypeVarPair *pair in mainFunction.declare.funVar.pairs){
+            [args addObject:[MFValue defaultValueWithTypeEncoding:[pair typeEncode]]];
+        }
+        [ORArgsStack push:args];
+        [(ORFunctionImp *)mainFunction execute:scope];
+    }
+}
+
++ (void)linkSystemFunctionsWithNodes:(NSArray <ORNode *>*)nodes scope:(MFScopeChain *)scope{
+    NSMutableArray <ORTypeVarPair *>*funcVars = [NSMutableArray array];
+    NSMutableArray *names = [NSMutableArray array];
+    for (id <OCExecute> expression in nodes) {
+        if ([expression isKindOfClass:[ORDeclareExpression class]]) {
+            ORTypeVarPair *pair = [(ORDeclareExpression *)expression pair];
+            //不能是Block声明或者是函数指针声明
+            if ([pair.var isKindOfClass:[ORFuncVariable class]]
+                && !pair.var.isBlock
+                && pair.var.ptCount == 0) {
+                [funcVars addObject:pair];
+                [names addObject:pair.var.varname];
+            }
+        }
     }
     //获取函数指针
     NSDictionary *table = [ORSearchedFunction functionTableForNames:names];
@@ -51,7 +77,7 @@
             [scope setValue:[MFValue valueWithObject:function] withIndentifier:function.name];
         }
     }
-    #if DEBUG
+#if DEBUG
     NSMutableArray *functionNames = [NSMutableArray array];
     for (ORTypeVarPair *pair in funcVars){
         NSString *functionName = pair.var.varname;
@@ -75,7 +101,8 @@
         }
         [build_ins appendString:@"-----------------------------------------------"];
         NSLog(@"%@", build_ins);
+        NSAssert(NO, @"");
     }
-    #endif
+#endif
 }
 @end
