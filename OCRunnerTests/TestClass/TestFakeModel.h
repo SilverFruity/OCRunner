7f493da928593cada55198d16de39502c0f2d6f2
carefree <946715806@qq.com>
2021-11-25 11:13:01 +0800

    添加MJExtension的测试用例

---
+++ OCRunnerTests/TestClass/TestFakeModel.h
@@ -0,0 +1,18 @@
+//
+//  TestFakeModel.h
+//  OCRunnerDemoTests
+//
+//  Created by carefree on 2021/11/25.
+//  Copyright © 2021 SilverFruity. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestFakeModel : NSObject
+@property (nonatomic, copy) id count;
+@property (nonatomic, strong) id sub;
+@end
+
+NS_ASSUME_NONNULL_END
